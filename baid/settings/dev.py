7bbd540ab391794ec9f705b196da5f7ff2a813fd
He <li@imlihe.com>
2023-06-08 08:40:17 +0800

    init

---
+++ baid/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h*^-yop-dw3a1vi69b62*ba3dj=p7a&f9wo6nl0t7on*!9!=3+"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ news/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
+from wagtail.admin.panels import FieldPanel
+from wagtail.api import APIField
+
+
+class NewsPage(Page):
+
+    # Lang is a multi selection field
+    lang = models.CharField(default="zh", max_length=2)
+
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('lang'),
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
+
+    api_fields = [
+        APIField('intro'),
+        APIField('lang'),
+        APIField('date'),
+        APIField('body'),
+        APIField('title')
+    ]
