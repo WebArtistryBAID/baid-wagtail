8119af0a76755c81cdeaef606aaee57532f77e7c
He <li@imlihe.com>
2023-06-10 17:42:37 +0800

    minor fixes

--- baid/settings/production.py
+++ baid/settings/production.py
@@ -1,6 +1,23 @@
 from .base import *
+import os
 
 DEBUG = False
+SECRET_KEY=os.environ["SECRET_KEY"]
+ALLOWED_HOSTS=["*"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
 
 try:
     from .local import *
